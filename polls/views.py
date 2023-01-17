465ddc48d5a8879c6a366e92374cc09a267a8b20
mnaeem <naeemshahzad7075@gmail.com>
2023-01-17 23:54:16 +0500

    polls - raise exception for error

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # from django.template import loader
+from django.http import Http404
 from django.shortcuts import render
 
 from .models import Question
@@ -13,7 +14,11 @@ def index(request):
   return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-  return HttpResponse("You're looking at question %s." % question_id)
+  try:
+    question = Question.objects.filter(pk=question_id)
+  except Question.DoesNotExist:
+    raise Http404("Question doesn't exist")
+  return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
   return HttpResponse("You're looking at the results of question %s." % question_id)
