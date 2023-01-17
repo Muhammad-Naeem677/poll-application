323b7e90d14930852e4ea0eccd8a7a92e3976df2
mnaeem <naeemshahzad7075@gmail.com>
2023-01-17 14:58:49 +0500

    polls - added 3 new views and configure their urls

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  # ex: /polls/
   path('', views.index, name='index'),
+  # ex: /polls/:id/
+  path('<int:question_id>/', views.detail, name='detail'),
+  # ex: /polls/:id/results/
+  path('<int:question_id>/', views.results, name='results'),
+  # ex: /polls/:id/vote/
+  path('<int:question_id>/', views.vote, name='vote'),
 ]
